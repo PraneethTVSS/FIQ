f8a7d335cda8c73962bb82cbe005e0dc3de72bdd
PraneethTVSS <tvss.praneeth@gmail.com>
2022-02-19 13:26:09 +0530

    Update and rename perms.py to generate_combinations.py

--- generate_combinations.py
+++ generate_combinations.py
@@ -5,6 +5,7 @@ input_data = raw_input("Enter your text: ")
 len_of_input = len(input_data)
 
 for combination_length in range(len_of_input):
+    # Generate the different length of combinations of strings
     combinations = itertools.combinations(input_data, combination_length)
     for combination in combinations:
         print("".join(combination))
